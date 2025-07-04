98ab86816bf9705cd78272984fa0a7f5400ed150
bryanbill <emobrya@gmail.com>
2025-07-05 00:38:01 +0300

    feat: add Dart bindings for ReactDOM interop classes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.0.3
+
+- Dartify ReactDOM interop classes
+
 ## 0.0.2
 
 - Dartify React interop classes
---
+++ lib/react-dom/index/index.dart
@@ -0,0 +1,10 @@
+// Library file for index
+export './container.dart';
+export './createroot.dart';
+export './do_not_use_or_you_will_be_fired_experimental_create_root_containers.dart';
+export './errorinfo.dart';
+export './hydrateroot.dart';
+export './hydrationoptions.dart';
+export './reactformstate.dart';
+export './root.dart';
+export './rootoptions.dart';
